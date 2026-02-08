6afbcd2fe5770eb015917928bfda110512f9b280
uros1velimirovic <uros1velimirovic@gmail.com>
2026-02-08 19:19:15 +0100

    proj-21-22-sem-ana-14-implementacija-3-tabela-simbola-3

--- src/rs/ac/bg/etf/pp1/SemAnalyzer.java
+++ src/rs/ac/bg/etf/pp1/SemAnalyzer.java
@@ -21,6 +21,7 @@ public class SemAnalyzer extends VisitorAdaptor {
 	private int constant;
 	private Struct constantType;
 	private Struct boolType = Tab.find("bool").getType();
+	private Obj currentMethod;
 
 /*----------------------------------------------------------------------------------------------------------------*/
 
@@ -62,9 +63,11 @@ public class SemAnalyzer extends VisitorAdaptor {
 		Tab.closeScope();
 		currentProgram = null;
 		
-//		if(mainMethod == null || mainMethod.getLevel() > 0)
-//			report_error("Program nema adekvatnu main metodu", program);
+		if(mainMethod == null) // || mainMethod.getLevel() > 0)
+			report_error("Program nema adekvatnu main metodu", program);
 	}
+	
+/*----------------------------------------------------------------------------------------------------------------*/
 
 	/* CONST DECLARATIONS */
 	
@@ -83,7 +86,6 @@ public class SemAnalyzer extends VisitorAdaptor {
 				report_error("Neadekvatna dodela konstanti: " + constDecl.getI1(), constDecl);
 			}
 		}
-		
 	}
 	
 	@Override
@@ -103,6 +105,70 @@ public class SemAnalyzer extends VisitorAdaptor {
 		constant = constant_bool.getB1();
 		constantType = boolType;
 	}
+	
+/*----------------------------------------------------------------------------------------------------------------*/
+
+	/* VAR DECLARATIONS */
+	
+	@Override
+	public void visit(VarDecl_var varDecl_var) {
+		Obj varObj = null;
+		if(currentMethod == null)
+			varObj = Tab.find(varDecl_var.getI1());
+		else
+			varObj = Tab.currentScope().findSymbol(varDecl_var.getI1());
+		
+		if(varObj == null || varObj == Tab.noObj) {
+			varObj = Tab.insert(Obj.Var, varDecl_var.getI1(), currentType);
+		}
+		else {
+			report_error("Dvostruka definicija promenljive: " + varDecl_var.getI1(), varDecl_var);
+		}
+	}
+	
+	@Override
+	public void visit(VarDecl_array varDecl_array) {
+		Obj arrayObj = null;
+		if(currentMethod == null)
+			arrayObj = Tab.find(varDecl_array.getI1());
+		else
+			arrayObj = Tab.currentScope().findSymbol(varDecl_array.getI1());
+		
+		if(arrayObj == null || arrayObj == Tab.noObj) {
+			arrayObj = Tab.insert(Obj.Var, varDecl_array.getI1(), new Struct(Struct.Array, currentType));
+		}
+		else {
+			report_error("Dvostruka definicija niza: " + varDecl_array.getI1(), varDecl_array);
+		}
+	}
+	
+/*----------------------------------------------------------------------------------------------------------------*/
+
+	/* METHOD DECLARATIONS */
+	
+	@Override
+	public void visit(MethodReturnTypeAndName_void methodReturnTypeAndName_void) {
+		currentMethod = Tab.insert(Obj.Meth, methodReturnTypeAndName_void.getI1(), Tab.noType);
+		Tab.openScope();
+		
+		if(methodReturnTypeAndName_void.getI1().equalsIgnoreCase("main"))
+			mainMethod = currentMethod;
+	}
+	
+	@Override
+	public void visit(MethodReturnTypeAndName_Type methodReturnTypeAndName_Type) {
+		currentMethod = Tab.insert(Obj.Meth, methodReturnTypeAndName_Type.getI2(), currentType);
+		Tab.openScope();
+	}
+	
+	@Override
+	public void visit(MethodDecl methodDecl) {
+		Tab.chainLocalSymbols(currentMethod);
+		Tab.closeScope();
+		currentMethod = null;
+	}
+	
+/*----------------------------------------------------------------------------------------------------------------*/
 	
 	@Override
 	public void visit(Type type) {
